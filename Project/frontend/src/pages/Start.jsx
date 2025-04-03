f40fcf0b3946982970cc4bc0b846f3a74355e066
ImanSamantaCoder <iman.samanta.code@gmail.com>
2025-04-03 12:56:31 +0530

    added collaborator section

--- Project/frontend/src/pages/Start.jsx
+++ Project/frontend/src/pages/Start.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import Navbar from "../component/Navbar";
 import Advertise from "../component/Advertise";
 import Footer from "../component/Footer";
+import Collaborator from "../component/Collaborator";
 const start = ()=>{
 
     return(
         <>
          <Navbar/>
          <Advertise/>
+         <Collaborator/>
          <Footer/>
         </>
     )
